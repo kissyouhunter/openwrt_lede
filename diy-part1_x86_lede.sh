a741b6ec79e652351eb41e046e55e58c0cede7be
kissyouhunter <63334206+kissyouhunter@users.noreply.github.com>
2021-12-07 23:00:05 +0800

    Update diy-part1_x86_lede.sh

--- diy-part1_x86_lede.sh
+++ diy-part1_x86_lede.sh
@@ -22,6 +22,7 @@ rm -rf ./package/lean/luci-app-netdata
 rm -rf ./package/lean/luci-app-jd-dailybonus
 rm -rf ./package/lean/luci-lib-docker
 rm -rf ./package/lean/luci-app-cpufreq
+rm -rf ./package/lean/luci-app-dockerman
 
 # 替换index.htm文件 X86
 wget -O ./package/lean/autocore/files/x86/index.htm https://raw.githubusercontent.com/kissyouhunter/openwrt_X86/main/diy/x86_lede/index_x86.htm
