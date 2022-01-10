a8c67b6e75d2aa78d17a3561ad325894cfd28d00
kissyouhunter <63334206+kissyouhunter@users.noreply.github.com>
2022-01-10 20:09:58 +0800

    update

--- diy-part1_x86_lede.sh
+++ diy-part1_x86_lede.sh
@@ -33,3 +33,9 @@ wget -O ./package/base-files/files/etc/banner https://raw.githubusercontent.com/
 # 替换内核
 sed -i 's/PATCHVER:=5.10/PATCHVER:=5.4/g' ./target/linux/x86/Makefile
 
+# 内核替换成 kernel 5.4.170
+
+sed -i 's/LINUX_KERNEL_HASH-5.4.162 = c12d72ddaac78189305a5e98825295ecb02282970033b052276035e83189e25b/LINUX_KERNEL_HASH-5.4.170 = b09f74e0cf5fc7cf5de6aa932fe654c962cb10118bdbbdddb397022c6e6d382c/g' ./include/kernel-version.mk
+
+sed -i 's/LINUX_VERSION-5.4 = .162/LINUX_VERSION-5.4 = .170/g' ./include/kernel-version.mk
+
