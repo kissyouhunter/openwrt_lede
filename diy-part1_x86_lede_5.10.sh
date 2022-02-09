e0857fc0a208121b265f42a923c9e35600e3bae7
kissyouhunter <kissyouhunter@hotmail.com>
2022-02-09 20:48:37 +0800

    update

--- diy-part1_x86_lede_5.10.sh
+++ diy-part1_x86_lede_5.10.sh
@@ -35,7 +35,7 @@ wget -O ./package/base-files/files/etc/banner https://raw.githubusercontent.com/
 
 # 内核替换成 kernel 5.10.90
 
-#sed -i 's/LINUX_KERNEL_HASH-5.10.94 = 28ff44aa4a9a8a1ea52ae74e448da6174ca4ff042ddc202e36b157c951cd7508/LINUX_KERNEL_HASH-5.10.95 = c8d9daf94e2784ddc9cbb49936c89a20415aa02faa2c4ab4a832ab6d8c63fc23/g' ./include/kernel-5.10
+sed -i 's/LINUX_KERNEL_HASH-5.10.98 = 1b24c6c15f1011bcca54da7e58539c612c7c84f95b7902f62d34754aaf0f9443/LINUX_KERNEL_HASH-5.10.99 = 00a53558aad3d8638bbe3bb69ed82a2b5b58ed3af01596bc18686cd04a790449/g' ./include/kernel-5.10
 
-#sed -i 's/LINUX_VERSION-5.10 = .94/LINUX_VERSION-5.10 = .95/g' ./include/kernel-5.10
+sed -i 's/LINUX_VERSION-5.10 = .98/LINUX_VERSION-5.10 = .99/g' ./include/kernel-5.10
 
