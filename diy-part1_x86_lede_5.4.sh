d743750e8fb7357f3a96e817f3f6dc80c9b9f101
kissyouhunter <kissyouhunter@hotmail.com>
2022-02-10 00:46:35 +0800

    update

--- diy-part1_x86_lede_5.4.sh
+++ diy-part1_x86_lede_5.4.sh
@@ -35,6 +35,6 @@ sed -i 's/PATCHVER:=5.10/PATCHVER:=5.4/g' ./target/linux/x86/Makefile
 
 # 内核替换成 kernel 5.4.175
 
-#sed -i 's/LINUX_KERNEL_HASH-5.4.175 = ac901bdffb1488d6c730ca7ab42322163dd331b240e2f06ad83d199e251a4840/LINUX_KERNEL_HASH-5.4.176 = d9600924a64e7a07a3a1bf2cf018d030ce9aec51805bb43330983364c968c740/g' ./include/kernel-5.4
+sed -i 's/LINUX_KERNEL_HASH-5.4.175 = ac901bdffb1488d6c730ca7ab42322163dd331b240e2f06ad83d199e251a4840/LINUX_KERNEL_HASH-5.4.178 = 4cdcab07decea7a818b8221f5b5ee5055300addc266b8300cd2a934cf1f762a6/g' ./include/kernel-5.4
 
-#sed -i 's/LINUX_VERSION-5.4 = .175/LINUX_VERSION-5.4 = .176/g' ./include/kernel-5.4
+sed -i 's/LINUX_VERSION-5.4 = .175/LINUX_VERSION-5.4 = .178/g' ./include/kernel-5.4
