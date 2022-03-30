f5d97845c4e09a100d19527061e0ade88ce6c220
kissyouhunter <63334206+kissyouhunter@users.noreply.github.com>
2022-03-30 18:49:27 +0800

    Update diy-part1_x86_lede_5.10.sh

--- diy-part1_x86_lede_5.10.sh
+++ diy-part1_x86_lede_5.10.sh
@@ -32,6 +32,5 @@ sed -i 's/PATCHVER:=5.15/PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
 #sed -i 's/LINUX_VERSION-5.10 = .100/LINUX_VERSION-5.10 = .101/g' ./include/kernel-5.10
 
-# 删除error patch
-
-rm -f ./target/linux/generic/pending-5.10/701-net-ethernet-mtk_eth_soc-add-ipv6-flow-offloading-support.patch
+# 暂时修复错误
+wget -O ./target/linux/generic/hack-5.10/781-dsa-register-every-port-with-of_platform.patch https://raw.githubusercontent.com/kissyouhunter/openwrt_lede/main/781-dsa-register-every-port-with-of_platform.patch
